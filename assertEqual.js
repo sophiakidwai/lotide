01ad3bc2abb268c570edeb6903ed424f369b7f51
sophia kidawai <sophia.kidwai@telus.com>
2022-11-14 17:33:48 -0800

    assertEqual assignment

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+//FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🥰🥰🥰Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("💩💩💩Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+
+//TEST CODE
+assertEqual("lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
