28f16960aa18ed4b7ab04a86de58e091eac11980
sophia kidawai <sophia.kidwai@telus.com>
2022-11-14 22:02:28 -0800

    fixing typo in code

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 //FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`🥰🥰🥰Assertion Passed: ${expected} === ${expected}`);
+    console.log(`🥰🥰🥰Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`💩💩💩Assertion Failed: ${actual} !== ${expected}`);
   }
