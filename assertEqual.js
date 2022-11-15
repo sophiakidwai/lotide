cc28db29a4e412d5d82cfbbc48f45eb56ace31b0
sophia kidawai <sophia.kidwai@telus.com>
2022-11-14 17:50:20 -0800

    updated with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 //FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🥰🥰🥰Assertion Passed: " + actual + " === " + expected);
+    console.log(`🥰🥰🥰Assertion Passed: ${expected} === ${expected}`);
   } else {
-    console.log("💩💩💩Assertion Failed: " + actual + " !== " + expected);
+    console.log(`💩💩💩Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
