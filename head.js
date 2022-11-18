3fa21684b23d7a8131bbfac0a997fe244052507b
sophia kidawai <sophia.kidwai@telus.com>
2022-11-17 23:29:56 -0800

    commiting linted version

--- head.js
+++ head.js
@@ -1,16 +1,16 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected){
-    console.log(`💚💚💚Assertion Passed: ${actual} === ${expected}`)
-  } else if (actual !== expected){
-  console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  if (actual === expected) {
+    console.log(`💚💚💚Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-let head = function(array){
+let head = function(array) {
   let first = array[0];
   return first;
-}
+};
 
 assertEqual(head([]), 5);
-assertEqual(head([9, 8, 7, 6]), 9)
+assertEqual(head([9, 8, 7, 6]), 9);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
