66da950d64e4a7baacaaaa305b1fedb28eee74c0
sophia kidawai <sophia.kidwai@telus.com>
2022-11-17 23:26:43 -0800

    commiting linted version

--- tail.js
+++ tail.js
@@ -1,25 +1,19 @@
-//FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`🥰🥰🥰Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`💩💩💩Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`💚💚💚Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
   }
-
 };
-
 const tail = function(array) {
   return array.slice(1);
 };
-
-
-//TEST CODE
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2);
 assertEqual(result[0], "Lighthouse");
 assertEqual(result[1], "Labs");
 
-//Test Case: Check the original array 
-/*const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!*/
+// Test Case: Check the original array
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
