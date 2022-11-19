e44db71bf4e985a41e78015f562efda0eacb03ff
sophia kidawai <sophia.kidwai@telus.com>
2022-11-19 00:01:09 -0800

    refactor eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -12,11 +12,10 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
     }
   }
   return true;
