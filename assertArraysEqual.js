257444b8d1e65be2b45954a8fd50ba5034129315
sophia kidawai <sophia.kidwai@telus.com>
2022-11-19 00:05:33 -0800

    eslint version

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,7 @@
+//Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
+//The message will be similar to that of assertEqual. In fact, you could refer to the code for assertEqual to help you implement this. Instead of doing a simple === check though, this assertion function will make use of your eqArrays function for array comparison.
+
+
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -8,16 +12,16 @@ const eqArrays = function(arr1, arr2) {
     }
   }
   return true;
- 
+
 };
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log(`${arr1} === ${arr2}`)
+    console.log(`${arr1} === ${arr2}`);
   } else {
-    console.log(`${arr1} !== ${arr2}`)
+    console.log(`${arr1} is not === to ${arr2}`);
   }
-}
+};
 
-// CALLING FUNCTION AS A TEST
-//return assertArraysEqual([2, 3, 4], [2, 3, 7]);
+//TEST CODE
+// return assertArraysEqual([2, 3, 4], [2, 3, 7]);
