29172daf8a8f57f402698c907d7f6ee7fc207139
sophia kidawai <sophia.kidwai@telus.com>
2022-11-19 15:51:59 -0800

    eqObjects

---
+++ eqObjects.js
@@ -0,0 +1,53 @@
+//Implement the definition for function eqObjects which will take in two objects and returns true or false, based on a perfect match.
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🥰🥰🥰Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`💩💩💩Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+
+};
+
+const eqObjects = function(object1, object2) {
+  const object1Array = Object.keys(object1);
+  const object2Array = Object.keys(object2);
+  if (object1Array.length !== object2Array.length) {
+    return false;
+  }
+  for (const key of object1Array) {
+    const value1 = object1[key];
+    const value2 = object2[key];
+    if (Array.isArray(value1) || Array.isArray(value2)) {
+      if (!eqArrays(value1, value2)) {
+        return false;
+      }
+    } else if (value1 !== value2) {
+      return false;
+    }
+  }
+  return true;
+};
+
+/* const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+eqObjects(ab, ba);
+
+const abc = { a: "1", b: "2", c: "3" };
+eqObjects(ab, abc);
+*/
+
