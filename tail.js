6755d5ff086a668c816de0dc59ad2f22a4ff93df
sophia kidawai <sophia.kidwai@telus.com>
2022-11-21 13:29:40 -0800

    refactor

--- tail.js
+++ tail.js
@@ -1,17 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💚💚💚Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 const tail = function(array) {
   return array.slice(1);
 };
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+
+
+module.exports = tail;
 
 // Test Case: Check the original array
 // const words = ["Yo Yo", "Lighthouse", "Labs"];
