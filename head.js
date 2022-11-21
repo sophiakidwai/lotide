29b92b228a0c59024865303a2eb89a7d67d0a7c5
sophia kidawai <sophia.kidwai@telus.com>
2022-11-21 13:18:41 -0800

    refactor

--- head.js
+++ head.js
@@ -1,16 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💚💚💚Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 let head = function(array) {
   let first = array[0];
   return first;
 };
 
-assertEqual(head([]), 5);
-assertEqual(head([9, 8, 7, 6]), 9);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
