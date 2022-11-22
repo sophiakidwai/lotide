f4e10e9dee1c5cbb62517580dbe01a4322271728
sophia kidawai <sophia.kidwai@telus.com>
2022-11-21 20:03:17 -0800

    refactor

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,4 +1,13 @@
-const assertEqual = require('./assertEqual');
+const assertEqual = function(actual, expected) {
+  if (actual === expected){
+    console.log(`💚💚💚Assertion Passed: ${actual} === ${expected}`)
+  } else if (actual !== expected){
+  console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+//const assertEqual = require('./assertEqual');
 
 const findKeyByvalue = function(object, val) {
   let result = undefined;
@@ -17,9 +26,9 @@ const bestTVShowsByGenre = {
   drama:  "The Wire"
 };
 
-// assertEqual(findKeyByval(bestTVShowsByGenre, "The Wire"), "drama");
-// assertEqual(findKeyByval(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByval(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByval(bestTVShowsByGenre, "That '70s Show"), undefined);
 
 
-module.exports = findKeyByvalue
+//module.exports = findKeyByvalue
 
--- test/findKeyByValueTest.js
+++ test/findKeyByValueTest.js
@@ -1,7 +1,7 @@
-const assertEqual = require('../assertEqual');
-const findKeyByValue = require('../findKeyByValue');
+// const assertEqual = require('../assertEqual');
+// const findKeyByValue = require('../findKeyByValue');
 
 
 
-assertEqual(findKeyByvalue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByvalue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// assertEqual(findKeyByvalue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByvalue(bestTVShowsByGenre, "That '70s Show"), undefined);
