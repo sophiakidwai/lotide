15c014e46b9f5ce87d5a1ed024d1a90395ede829
sophia kidawai <sophia.kidwai@telus.com>
2022-11-21 22:09:05 -0800

    refactor

--- countLetters.js
+++ countLetters.js
@@ -1,10 +1,9 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🥰🥰🥰Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`💩💩💩Assertion Failed: ${actual} !== ${expected}`);
+  if (actual === expected){
+    console.log(`💚💚💚Assertion Passed: ${actual} === ${expected}`)
+  } else if (actual !== expected){
+  console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
   }
-
 };
 
 const countLetters = function(str){
